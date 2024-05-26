ef9c5ab30ce2372456f5c1ed93d3fc3df8775438
samik1987 <samik.net@gmail.com>
2024-05-26 15:14:30 +0530

    Practice with calculation Commit

---
+++ .venv/main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+birth_year = input("Enter your birth year : ")
+age = 2024 - int(birth_year)
+print("your current age is =", age)
+print("Lets do some math ...")
+first_number =float(input("Enter First Number : "))
+second_number =float(input("Enter Second Number : "))
+sum = first_number + second_number
+print("Total is =", sum)
+
+
